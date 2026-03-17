be20beb820ae92e62172c3733da779152b93278f
Jose Greorio <cryptodental7@gmail.com>
2026-03-17 23:18:26 +0100

    landing ok

---
+++ src/app/newFile.tsx
@@ -0,0 +1,8 @@
+(
+  <main>
+    className="min-h-screen
+    bg-white text-black flex
+    flex-col <items-center>
+      justify-center p-10"    )
+  }
+      )</></>);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-yellow-500">
+    <main className="min-h-screen bg-white text-black flex flex-col items-center justify-center p-10">
+
+      <h1 className="text-5xl font-bold text-yellow-500 mb-4">
         ORTHONOBA
       </h1>
+
+      <p className="text-xl mb-6 text-center max-w-xl">
+        Especialistas en dispositivos intraorales para apnea del sueño, deporte y ortodoncia.
+      </p>
+
+      <a href="#" className="bg-black text-white px-6 py-3 rounded-lg">
+        Contactar
+      </a>
+
     </main>
   );
 }

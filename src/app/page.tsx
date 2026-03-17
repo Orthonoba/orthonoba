ef96a454f0c48358e0168b7204c5b2e4d3db3b9a
Jose Greorio <cryptodental7@gmail.com>
2026-03-17 20:59:53 +0100

    Primer commit ORTHONOBA

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-yellow-500">
+        ORTHONOBA
+      </h1>
+    </main>
+  );
+}
